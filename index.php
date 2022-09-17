fa89364f661905b4f4ea2735a12de8c3507b3a96
masaka953 <kariem4869@gmail.com>
2022-09-17 19:09:50 +0700

    ADDED

---
+++ index.php
@@ -0,0 +1,28 @@
+<h3> Tambah Barang </h3>
+
+<form action="" method="post">
+<table>
+    <tr>
+        <td width="120"> Kode Barang </td>
+        <td> <input type="text" name="satu"> </td>
+    </tr>
+    <tr>
+        <td></td>
+        <td><input type="submit" name="proses" value="Simpan"> </td>
+    </tr>
+</table>
+
+</form>
+
+<?php
+include "koneksi.php";
+
+if(isset($_POST['proses'])){
+mysqli_query($koneksi, "insert into cobacoba set  
+satu = '$_POST[satu]'");
+
+echo "Data barang baru telah tersimpan";
+
+}
+
+?>
